f2ebbff8bbf182dd0c8b59a1ae4fc9660269f36d
ofix <shb8845369@gmail.com>
2024-04-09 14:01:47 +0800

    update content_service.js to support communication

--- src/content_scripts/content.js
+++ src/content_scripts/content.js
@@ -3,12 +3,12 @@ const init = () => {
     const inject_iframe = document.createElement("iframe");
     inject_iframe.id = id;
     inject_iframe.style.cssText =
-      "width: 100%; height: 100%; position: fixed; inset: 0px; margin: 0px auto; z-index: 10000002; border: none;";
+      "width: 100%; height: 40%; position: fixed; left:-100%; top:60%; z-index: 10000002; border: none;";
     const page_src = chrome.runtime.getURL(page_url);
     inject_iframe.src = page_src;
     document.body.appendChild(inject_iframe);
   };
-
+  console.log("inject iframe content page");
   // 注入iframe页面
   injectContentPage("content-start-iframe", "content_pages/index.html");
 };
